e74d00a2032cd2e41dfc50cbb2f12ab6028b1017
Виктория <maximum1234@ya.ru>
2023-07-11 20:50:10 +0300

    дз_4

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),path('',include('app_advertisements.urls'))
+    path('admin/', admin.site.urls),
+    path('lesson_4/',include('app_lesson_4.urls')),
+    path('ap/',include('app_advertisements.urls'))
 ]
---
+++ app_lesson_4/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from app_lesson_4.views import index
+
+urlpatterns=[path('',index)]
