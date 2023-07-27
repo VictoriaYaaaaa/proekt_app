c9a909f102074fc6c9e74742e9e417eead5d2d43
Виктория <maximum1234@ya.ru>
2023-07-27 21:00:28 +0300

    1 коммит

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField("заголовок",max_length=128)
+    description=models.TextField("описание")
+    price=models.DecimalField("цена",max_digits=10, decimal_places=2)
+    auction=models.BooleanField("торг",help_text="Если торг уместен, то True(1), если не уместен - False(0)")
+    created_data=models.DateTimeField("дата публикации",auto_now_add=True)
+    updated_data=models.DateTimeField("дата обновления",auto_now=True)
+    
