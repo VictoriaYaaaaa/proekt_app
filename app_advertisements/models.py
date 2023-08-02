b807856855f9e1893af631a05c3983e1b0281a32
Виктория <maximum1234@ya.ru>
2023-08-02 17:24:19 +0300

    дз 7

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -9,4 +9,9 @@ class Advertisement(models.Model):
     auction=models.BooleanField("торг",help_text="Если торг уместен, то True(1), если не уместен - False(0)")
     created_data=models.DateTimeField("дата публикации",auto_now_add=True)
     updated_data=models.DateTimeField("дата обновления",auto_now=True)
+
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
     
+    class Meta:
+        db_table="advertisements"
