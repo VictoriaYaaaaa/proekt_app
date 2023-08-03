a8671aaae83ce8133383d5fb9ded41ad7d158182
Виктория <maximum1234@ya.ru>
2023-08-03 21:12:35 +0300

    первый коммит

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+import .models import Advertisement
 # Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id','title','description','price','created_date','auction']
+    list_filter= ['auction','price','created_data','id']
+    auction=['make_auction_as_false','make_auction_as_true']
+
+    @admin.action(description="Убрать возможность торга ")
+    def make_auction_as_false (self,request,queryset):
+        queryset.update(auction=False)
+
+    @admin.action(description="Добавить возможность торга ")
+    def make_auction_as_true (self,request,queryset):
+        queryset.update(auction=True)
+    
+    fieldsets=(
+        ("Общее",{'fields':('title','description')}),
+        ("Финансы",{'fields':('auction','price'),'classes':['collapse']})
+    )
+
+
+admin.site.register(Advertisement,AdvertisementAdmin)
+    
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html 
 
 # Create your models here.
 
@@ -13,5 +15,12 @@ class Advertisement(models.Model):
     def __str__(self):
         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
     
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_data.date()==timezone.now().date():
+            created_time=self.created_data.time().strftime("%H:%M:%S")
+            return format_html('<span style="color:orange;font-weight:bold;">Сегодня в {}</span>',created_time)
+        return self.created_data.strftime("%d.%m.%Y в %H:%M:%S")          
+        
     class Meta:
         db_table="advertisements"
