5ef240759b4447e786841a44e54cf1b1009a3f4e
Виктория <maximum1234@ya.ru>
2023-08-22 16:50:50 +0300

    формы

--- app_advertisements/forms.py
+++ app_advertisements/forms.py
@@ -1,8 +1,24 @@
 from django import forms
+from django.db import models
+from .models import Advertisement
 
-class AdvertisementForm(forms.Form):
-    title=forms.CharField(max_length=64,widget=forms.TextInput(attrs={'class':'form-control form-control-lg'}))
-    description=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg'}))
-    price=forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control form-control-lg'}))
-    auction=forms.BooleanField(required=False,widget=forms.CheckboxInput(attrs={'class':'form-check-input'}))
-    image=forms.ImageField(widget=forms.FileInput(attrs={'class':'form-control form-control-lg'}))
+class AdvertisementForm(forms.ModelForm):
+    class Meta:
+        model = Advertisement
+        fields=('title','description','price','auction','image')
+        widgets= {
+            'title':forms.TextInput(attrs={'rows':'10', 'class':'form-control form-control-lg'}),
+            'description':forms.Textarea(attrs={'class':'form-control form-control-lg'}),
+            'price':forms.NumberInput(attrs={'class':'form-control form-control-lg'}),
+            'auction':forms.CheckboxInput(attrs={'class':'form-check-input'}),
+            'image':forms.FileInput(attrs={'class':'form-control form-control-lg'})
+            }
+    def clean_title(self):
+        data=self.cleaned_data['title']
+        if data[0]=='?':
+            raise forms.ValidationError("Заголовок не может начинаться с вопросительного знака, передалайте!")
+        return data
+        
+
+
+
